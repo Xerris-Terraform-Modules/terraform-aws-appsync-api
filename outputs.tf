f1a2e3b5050eb10f456719d7f5173bb92712311d
Dariem Garcés <catdemian@gmail.com>
2021-04-17 19:00:22 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "appsync_graphqluri" {
   description = "ID of created appsync"
   value = aws_appsync_graphql_api.foe_api.uris["GRAPHQL"]
 }
+
+output "endpoint" {
+  description = "The endpoint for the API."
+  value       = data.external.graphql_endpoint.result["endpoint"]
+}
